f60b241cede536000e6f6d685ef2c73804718a54
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-02-16 20:33:14 +0000

    did some work, not working though

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,24 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def backdoor
+    @url = params["url"]
+    @desc = params["desc"]
+    @thumb = params["thumb"]
+
+    render({ :template => "item_templates/backdoor" })
+  end
+
+  def create
+    @url = params["url"]
+    @desc = params["desc"]
+    @thumb = params["thumb"]
+
+    curr = Item.new
+    curr.link_description = @desc
+    curr.link_url = @url
+    curr.thumbnail_url = @thumb
+    redirect_to("/")
+  end
 end
